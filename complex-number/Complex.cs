3a4c1484819ede754b21a002a2e3d049e77b0973
MichkaDaCoder <michkadacoder@gmail.com>
2022-11-06 12:54:39 +0100

    first commit

---
+++ complex-number/Complex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complex_number
+{
+    /// <summary>
+    /// Complex.cs
+    /// Author: MichkaDaCoder
+    /// Date: 06.11.2022
+    /// </summary>
+    public class Complex
+    {
+        /// <summary>
+        /// Real part
+        /// </summary>
+        public double Re { get; set; }
+
+        /// <summary>
+        /// Imaginary part
+        /// </summary>
+        public double Im { get; set; }
+
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="Re"></param>
+        /// <param name="Im"></param>
+        public Complex(double Re, double Im)
+        {
+            this.Re = Re;
+            this.Im = Im;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Re);
+            sb.Append(Im > 0 ? "+" + Im+"i" : Im + "i");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Return the sum of 2 complex numbers
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns>Complex</returns>
+        public Complex Sum(Complex c1, Complex c2)
+        {
+            return new Complex(c1.Re + c2.Re, c1.Im + c2.Im);
+        }
+
+        /// <summary>
+        /// Return the sub of 2 complex numbers
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public Complex Sub(Complex c1, Complex c2)
+        {
+            return new Complex(c1.Re - c2.Re, c1.Im - c2.Im);
+        }
+    }
+}
---
+++ complex-number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complex_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "complex-number";
+
+            Complex complex1 = new Complex(2, 3);
+            Complex complex2 = new Complex(3, 4);
+
+            Console.WriteLine("First complex : " + complex1);
+            Console.WriteLine("Second complex: " + complex2);
+            Console.WriteLine();
+            Console.WriteLine("Sum complex: " + complex1.Sum(complex1, complex2));
+            Console.WriteLine("Sub complex: " + complex1.Sub(complex1, complex2));
+            Console.ReadKey(true);
+        }
+    }
+}
